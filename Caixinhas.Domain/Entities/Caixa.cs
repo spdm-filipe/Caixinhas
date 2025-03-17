e949502053eaaffabd919752f2b55ad40fe904fa
Filipe Augusto Santos Chaves <filipe.chaves@spdm-pais.org.br>
2025-03-17 11:39:33 -0300

    entities

--- Caixinhas.Domain/Entities/Caixa.cs
+++ Caixinhas.Domain/Entities/Caixa.cs
@@ -1,4 +1,7 @@
-﻿namespace Caixinhas.Domain.Entities;
+﻿using Caixinhas.Domain.Validation;
+using System.Drawing;
+
+namespace Caixinhas.Domain.Entities;
 
 public class Caixa : Entity
 {
@@ -7,8 +10,33 @@ public class Caixa : Entity
     public bool IsAtivo { get; private set; }
     public string  Descricao { get; private set; } =string.Empty;
     public DateTime Prazo { get; private set; }
-    public int IdUsuarioDono { get; private set; }
+    public int IdUsuario { get; private set; }
 
+    public Caixa(int idUsuario, decimal valorAtual, decimal valorMaximo,
+        string descricao, DateTime prazo)
+    {
+        ValidateDomin(valorAtual, valorMaximo, descricao, prazo);
+        IdUsuario = idUsuario;
+    }
+    public void Update(int valorAtual, int valorMaximo, string descricao,
+        string prazo)
+    {
+        ValidateDomin(valorAtual, valorMaximo, descricao, prazo);
+    }
 
+    private void ValidateDomin(decimal valorAtual, decimal valorMaximo, string descricao,
+       DateTime prazo)
+    {
+        DomainExceptionValidation.When(valorAtual < 0, "Valor atual inválido");
+        DomainExceptionValidation.When(valorMaximo < 0, " Valor máximo inválido");
+        DomainExceptionValidation.When(descricao.Length > 200, "Descrição inválida");
+        DomainExceptionValidation.When(string.IsNullOrEmpty(descricao), "Descrição inválida");
+        DomainExceptionValidation.When(prazo < DateTime.Now, "Prazo inválido");
 
+        Descricao = descricao;
+        ValorAtual = valorAtual;
+        ValorMaximo = valorMaximo;
+        Prazo = prazo;
+
+    }
 }
--- Caixinhas.Domain/Entities/CaixaLancamento.cs
+++ Caixinhas.Domain/Entities/CaixaLancamento.cs
@@ -1,12 +1,36 @@
-﻿namespace Caixinhas.Domain.Entities;
+﻿using Caixinhas.Domain.Validation;
+
+namespace Caixinhas.Domain.Entities;
 
 public class CaixaLancamento : Entity
 {
     public int IdCaixa { get; private set; }
+    public Caixa Caixa { get; set; }
     public int IdUsuarioLancamento { get; private set; }
     public decimal Valor { get; private set; }
     public string Descricao { get; private set; } = string.Empty;
-    public bool IsExcluido { get; private set; }
 
+
+    public void Update(int idCaixa, int idUsuario, decimal valor,
+        string descricao)
+    {
+        ValidateDomin(idCaixa, idUsuario, Valor, descricao);
+    }
+
+    private void ValidateDomin(int idCaixa, int idUsuario, decimal valor,
+        string descricao)
+    {
+        DomainExceptionValidation.When(idCaixa < 0, "Id do caixa inválido");
+        DomainExceptionValidation.When(idUsuario < 0, "Id do usuário inválido");
+        DomainExceptionValidation.When(valor <= 0, "Valor inválido");
+        DomainExceptionValidation.When(descricao.Length > 200, "Descrição inválida");
+        DomainExceptionValidation.When(string.IsNullOrEmpty(descricao), "Descrição inválida");
+
+        Descricao = descricao;
+        IdCaixa = idCaixa;
+        IdUsuarioLancamento = idUsuario;
+        Valor = valor;
+    }
+ 
 
 }
